2b1289a04cfe205422d72e854c45c2e87f585aea
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2025-08-31 21:01:49 -0400

    feat: agrega schema change password XD

---
+++ src/schemas/schemaChangePassword.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const formSchemaChangePassword = z.object({
+    currentPassword: z.string(),
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // el error se asigna al campo confirmPassword
+  });
+
+export type ChangePasswordFormValues = z.infer<typeof formSchemaChangePassword>
